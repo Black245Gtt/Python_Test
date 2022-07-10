c86df5ccace9fe89c1aa9c80c7fed05a37da512f
Black245Gtt <105663555+Black245Gtt@users.noreply.github.com>
2022-07-11 00:13:24 +0300

    Add files via upload

---
+++ Tema_5/5.1.py
@@ -0,0 +1,9 @@
+
+def Perimeter(n, b, a, d):
+    return n+a+b+d
+n = float(input('1: '))
+b = float(input('2: '))
+a = float(input('3: '))
+d = float(input('4: '))
+res = Perimeter(n, b, a, d)
+print ('P =', res  )
