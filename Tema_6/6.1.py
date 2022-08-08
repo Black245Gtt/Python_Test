abaea5d0733444e4e934d0b8d6d71a034397724e
Black245Gtt <105663555+Black245Gtt@users.noreply.github.com>
2022-08-08 14:44:58 +0300

    Add files via upload

---
+++ Tema_6/6.1.py
@@ -0,0 +1,19 @@
+class Parallelepiped:
+    def __init__(self, length, width, height):
+        self.length = length
+        self.width = width
+        self.height = height
+
+    def volume(self):
+        return f'Объем: {self.length * self.width * self.height}'
+
+    def square(self):
+        return f'Площадь основания: {2 * (self.height * self.length + self.length * self.width + self.height * self.width)}'
+
+    def side (self):
+        return f'Площадь боковой стороны: {2 * (self.height * self.length + self.height * self.width)}'
+
+p = Parallelepiped(10, 5, 20)
+print(p.volume())
+print(p.square())
+print(p.side())
